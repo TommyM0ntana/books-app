5a75b70ebf72401eaa3ffe9941d4ef3b975dd47d
Tommy M0ntana <tommy.montana.harwood@gmail.com>
2020-01-21 18:54:03 +0100

    add models controller view(basic initials)  bcrypt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def new
+    #render a form where one can log in 
+  end
+  
+  def create
+    #takes params from new and logs the users in.
+  end
+  
+  def destroy
+#log the user out
+
+  end
+  
+  end
---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+
+  
+  def welcome
+    #render welcome page
+    #if not loged in prompts to log in or sign up
+    #if logged in redirect the user to their showpage page
+  end
+  
+  
+  end

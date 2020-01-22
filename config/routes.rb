6496d941abf771be0df9f9f17b356fca9fda27e2
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-01-22 16:36:12 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+root to: 'static#welcome'
+#main page is the static page o welcome page as root url home page
 resources :users, only: [:new, :create, :show]
+resources :sessions, only: [:new, :create, :destroy]
 
 
 end
